0f0ffb4312cae1f4c7fdc06e3230cbbe92f45272
LukeStorry <luke.storry@gmail.com>
2013-11-06 17:39:17 +0000

    Update Babylonian.pas
    Most of the program is there now, I just need to put the actual algorithm in the Repeat Loop.

--- BabylonianMethod/Babylonian.pas
+++ BabylonianMethod/Babylonian.pas
@@ -1,14 +1,15 @@
 //Written By Luke Storry
 //Part of the RootExtractors Project
-
 program Babylonian;
-
-vars
-      ;
-
-
-
+var   S,x,e:Integer ; //S is the input, x the best estimate, and e the error
 begin
+  Writeln('Root Extractor using the Babylonian method.') ;
+  Writeln('Written by Luke Storry'); Writeln;Writeln;
+  Writeln('Please enter a number');
+  Readln(S)
+  x:=S/2 //sets up the estimate variable
+  Repeat
 
-
+  until (x^2 - S)^2 < 0.001; //keep going untill estimate is accurate
+  Writeln('The square root of ',S,' is ',x);
 end.
