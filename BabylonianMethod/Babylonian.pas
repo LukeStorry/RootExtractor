9fcc1185beceb3ae98777673a87c6e7781c20313
LukeStorry <luke.storry@gmail.com>
2013-11-07 12:50:11 +0000

    Update Babylonian.pas
    Finished algorithm, added dp chooser

--- BabylonianMethod/Babylonian.pas
+++ BabylonianMethod/Babylonian.pas
@@ -1,15 +1,20 @@
 //Written By Luke Storry
 //Part of the RootExtractors Project
 program Babylonian;
-var   S,x,e:Integer ; //S is the input, x the best estimate, and e the error
+var   Num,x:Real; //Num is  input, x is estimate
+         dp:Byte; //decimal places to print the output to
 begin
   Writeln('Root Extractor using the Babylonian method.') ;
   Writeln('Written by Luke Storry'); Writeln;Writeln;
-  Writeln('Please enter a number');
-  Readln(S)
-  x:=S/2 //sets up the estimate variable
+  Write('Please enter a number:  ');
+  Readln(Num);
+  Write('Please enter how many decimal places:  ');
+  Readln(dp);
+  x:=Num/2; //sets up the estimate variable
   Repeat
-
-  until (x^2 - S)^2 < 0.001; //keep going untill estimate is accurate
-  Writeln('The square root of ',S,' is ',x);
+    x:=((x+(Num/x))/2); //set the new x as the average of x and Num/x
+    Writeln(x:8:dp);
+  until abs(Num-x*x)<1/exp(10*ln(dp)); //keep going until accurate to 10^-dp
+  Writeln('The square root of ',Num:1:dp,' is ',x:1:dp); //ouput
+  Readln;
 end.
